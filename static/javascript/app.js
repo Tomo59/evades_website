c448ccc657e689d6b9adf7ce0ed88faed2c1bc1a
Baptiste Coulange <bc@cornis.fr>
2014-04-21 09:52:21 +0200

    ajout de liste de spectacle dynamique

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,6 +1,54 @@
 App = Ember.Application.create({});
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
 
 App.Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.resource('spectacle', {path: '/spectacles/:date'});
+});
+
+
+App.Spectacle = DS.Model.extend({
+  title: DS.attr('string'),
+  date: DS.attr('date'),
+  place: DS.attr('string'),
+  message: DS.attr('string')
+});
+
+
+App.Spectacle.FIXTURES = [
+ {
+   id: 1,
+   title: 'Les évadés à Nancy',
+   date: "2014-04-21T09:33:03+02:00",
+   place: 'Nancy',
+   message: "Viendez nous voir à Nancy"
+ },
+ {
+   id: 2,
+   title: '...',
+   date: "2014-04-21T09:33:03+02:00",
+   place: 'Nancy',
+   message: "Viendez nous voir à Nancy"
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   date: "2014-04-21T09:33:03+02:00",
+   place: 'Nancy',
+   message: "Viendez nous voir à Nancy" 
+ }
+];
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('spectacle');
+  }
+});
+
+App.SpectacleRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.Spectacle.FIXTURES.findBy('date',params.date);
+  }
 });
