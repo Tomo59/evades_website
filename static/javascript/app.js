29979a71f00004b5a612baab37eefe3893fdad74
Thomas Gambier <thomas.gambier@gmail.com>
2015-03-11 07:52:39 +0100

    Override escapees route so that it calls fotorama script

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,9 +1,21 @@
-App = Ember.Application.create({});
+App = Ember.Application.create({
+  // put this to true if you want to lofg the transitions between different routes
+  LOG_TRANSITIONS: false
+});
 
 App.Router.map(function() {
-  this.route('escapees');
+  this.route('escapees', { path: '/evades' });
   this.route('videos');
-  this.route('accomplices');
+  this.route('accomplices', { path: '/complices' });
   this.route('about');
   this.route('contact');
+});
+
+// need to call fotorama javascript each time we land into escapees route
+App.EscapeesRoute = Ember.Route.extend({
+  renderTemplate: function(controller, model) {
+    this._super(controller, model);
+    console.log("have done the rendering, will call fotorama...");
+    Ember.$.getScript('http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.js');
+  }
 });
